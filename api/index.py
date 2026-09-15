6aa46c0a7db24fd0d934034c4051f4bff6732150
abdulselamahemade608-prog <abdulselamahemade608@gmail.com>
2026-09-15 15:41:12 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -16,7 +16,7 @@ from telebot import types
 # =========================================================
 
 TOKEN = os.environ.get("BOT_TOKEN", "PASTE_YOUR_BOT_TOKEN_HERE")
-ADMIN_IDS = [7725001366]
+ADMIN_IDS = [7725001366, 7948454166]
 CHANNELS = ["@felafel_arafa"]
 CHANNEL_URL = "https://t.me/Felafel_arafa"
 
